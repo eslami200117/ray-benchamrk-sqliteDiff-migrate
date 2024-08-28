c67521da00f503968586f124ceae92f9be472551
eslami <ab.eslami2001@gmail.com>
2024-08-28 16:03:42 +0330

    feat: add time stuff for benchmark

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/eslami200117/sqliteDiff/sqliteDiff"
 )
 
+func main() {
+	start := time.Now()
 
-func main(){
 	sqliteDiff.GetDiff("database/databases/pointsMaster", "database/databases/editedd")
+
+	duration := time.Since(start)
+
+	fmt.Printf("Execution time: %v\n", duration)
 }
