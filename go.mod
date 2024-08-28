9a27c6b4aec05fdf8895e8260f593c72a83e27fa
eslami <ab.eslami2001@gmail.com>
2024-08-28 08:02:18 +0330

    feat: add sqlite3 to project

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/eslami200117/sqliteDiff
 
 go 1.22.5
 
-require (
-
-)
+require github.com/mattn/go-sqlite3 v1.14.22
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-
-
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "/database/databases/pointsMaster")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
 
+	defer db.Close()
 }
