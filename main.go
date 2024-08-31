c0c2a9afee08228e3dd227a479dcaab91a7ccf94
eslami <ab.eslami2001@gmail.com>
2024-08-31 08:29:22 +0330

    feat: get arg from terminal

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
@@ -8,9 +9,21 @@ import (
 )
 
 func main() {
+	db1 := flag.String("db1", "", "Path to the first database file")
+	db2 := flag.String("db2", "", "Path to the second database file")
+
+	// Parse the flags
+	flag.Parse()
+
+	if *db1 == "" || *db2 == "" {
+		fmt.Println("Both db1 and db2 arguments are required")
+		flag.Usage()
+		return
+	}
+
 	start := time.Now()
 
-	name := sqliteDiff.GetDiff("database/databases/Empty", "database/databases/1403")
+	name := sqliteDiff.GetDiff(*db1, *db2)
 	duration := time.Since(start)
 	fmt.Printf("get diff time: %v\n", duration)
 
@@ -21,5 +34,4 @@ func main() {
 	sqliteDiff.ApplySql(name)
 	duration = time.Since(start)
 	fmt.Printf("total time: %v\n", duration)
-
 }
